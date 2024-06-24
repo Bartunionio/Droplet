a2bcd5194f55c5062167744efba92787f89d6793
Bartunionio <164276330+Bartunionio@users.noreply.github.com>
2024-06-24 02:30:00 +0200

    Add files via upload

--- Droplet/Controllers/HomeController.cs
+++ Droplet/Controllers/HomeController.cs
@@ -1,17 +1,25 @@
+using Droplet.Data;
 using Droplet.Models;
+using Droplet.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
-
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Droplet.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -24,9 +32,25 @@ namespace Droplet.Controllers
             return View();
         }
 
-        public IActionResult Blood_status()
+        // GET: Blood Status
+        [Route("Views/Home/Blood_status.cshtml", Name = "blood_status")]
+        public async Task<IActionResult> BloodStatus()
         {
-            return View();
+            var currentDate = DateOnly.FromDateTime(DateTime.Now);
+            var cutOffDate = currentDate.AddDays(-35);
+
+            var bloodStatus = await _context.Donations
+                .Include(d => d.Donor)
+                .Where(b => b.IdTransfusion == null && b.Date > cutOffDate)
+                .GroupBy(b => b.Donor.BloodType)
+                .Select(g => new
+                {
+                    BloodType = g.Key,
+                    Quantity = g.Count()
+                })
+                .ToListAsync();
+
+            return View("Views/Home/Blood_status.cshtml", bloodStatus);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
